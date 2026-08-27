ec049c41feb201c6c5cfa027a3309e51672ccb77
Daniel Londoño <danielfelond@gmail.com>
2026-08-26 19:39:46 -0500

    Add domain entities, AppDbContext and initial migration (Module 2)

---
+++ GolBet.Entities/Enums/MatchStatus.cs
@@ -0,0 +1,7 @@
+﻿public enum MatchStatus
+
+{
+    Scheduled = 0,
+    InProgress = 1,
+    Finished = 2
+}
--- GolBet.Web/Program.cs
+++ GolBet.Web/Program.cs
@@ -1,7 +1,12 @@
+using GolBet.Repositories.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -15,11 +20,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");

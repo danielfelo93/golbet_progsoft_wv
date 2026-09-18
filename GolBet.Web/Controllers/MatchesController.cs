30b60bb07991f76d6ef44a8b4378a2a16ddc600b
Daniel Londoño <danielfelond@gmail.com>
2026-09-18 13:33:40 -0500

    Add match detail page and board filters (Module 5)

--- GolBet.Web/Controllers/MatchesController.cs
+++ GolBet.Web/Controllers/MatchesController.cs
@@ -1,4 +1,5 @@
-﻿using GolBet.Services.Interfaces;
+﻿using GolBet.Entities.Enums;
+using GolBet.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GolBet.Web.Controllers;
@@ -6,13 +7,25 @@ namespace GolBet.Web.Controllers;
 public class MatchesController : Controller
 {
     private readonly IMatchService _matchService;
+
     public MatchesController(IMatchService matchService)
         => _matchService = matchService;
 
-    // GET /Matches 
-    public async Task<IActionResult> Index()
+    // GET /Matche -> all matches 
+    // GET /Matches?status=Scheduled -> filtered board 
+    public async Task<IActionResult> Index(MatchStatus? status)
     {
-        var board = await _matchService.GetBoardAsync();
+        ViewBag.CurrentStatus = status;
+        var board = await _matchService.GetBoardAsync(status);
         return View(board);
     }
+
+    // GET /Matches/Detail/3 
+    public async Task<IActionResult> Detail(int id)
+    {
+        var match = await _matchService.GetDetailAsync(id);
+        if (match is null) return NotFound();   // HTTP 404 
+        return View(match);
+    }
 }
+
